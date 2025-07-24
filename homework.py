5f84a6a69b56428907e675499ec89b915d36fe3b
temujinsalajan <temujin@salajan.com>
2025-07-24 15:09:10 +0800

    test

---
+++ homework.py
@@ -0,0 +1,13 @@
+name = ('Temujin')
+print(f"My name is {name}")
+age=11 
+print(f"My age is {age}")
+height=12.5      #so this is already like step 3 and 9
+print(f"My heigth is {height} ")
+is_student=True   #aswell this is like a empty boolean and i did not understood step 7-8 nad like 
+print(f"I am a {is_student}")
+
+city=46
+city_2=67
+print(f"In Alberqurque city the temupeture is {city}, and for Dubai city the tempeture is {city_2}") 
+
